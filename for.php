86df9b0788f0e64e1643324d89b640196bcfa766
iris <110B16890@mailst.cjcu.edu.tw>
2022-01-24 22:30:33 +0800

    adjust list

--- for.php
+++ for.php
@@ -3,26 +3,27 @@
     <meta charset="utf-8">
 </head>
 <body>
+
 <?php
     echo "<form action=\"for.php\">";
     echo "<input type=\"number\"  name=\"multiplier\">";
     echo "<input type=\"submit\">";
+    echo "</form>";
     $multiplier=$_GET['multiplier'];
     echo "請輸入2~9的其中一個數字"."<br/>";
     if ($multiplier>=2 and $multiplier<=9){
+        echo "<ul>";
         for($i=1;$i<1000;$i++){
-            $quotient=$multiplier*$i;
-            echo "<ul>
-            <li> $multiplier*$i=$quotient </li>
-            </ul>"."<br/>"; 
-            if ($i % 10 ==0){
-                echo "<br/>";
-            }  
-        }
-       
-    } else{
-        echo "您輸入的數字不在範圍內";
+        $quotient=$multiplier*$i;
+        echo "<li>$multiplier*$i=".$quotient."</li>";
+        if ($i % 10 ==0){
+            echo "<br/>";
+        }  
     }
+        }else{
+        echo "您輸入的數字不在範圍內";
+        }
 ?>
+</ul>
 </body>
 </html>
